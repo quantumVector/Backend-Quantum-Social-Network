eaddb8827810250c968eb208551a065fbc276028
quantumVector <payned@mail.ru>
2021-01-15 13:17:03 +0300

    Создание index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// разрешить CORS запросы Open Server
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
+//----------------------------------------------------------
+
+echo 'Hello world';
+?>
