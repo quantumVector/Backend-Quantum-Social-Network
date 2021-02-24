3f71c4cea91bd6eaeb07cf83b5ff8dcf67f0b7d3
quantumVector <payned@mail.ru>
2021-02-24 08:53:35 +0300

    Усовершенствована отправка списка друзей клиенту с учётом пагинации и гет параметров

--- scripts/get_friends_list.php
+++ scripts/get_friends_list.php
@@ -11,20 +11,38 @@ header('Access-Control-Allow-Credentials: true');
 
 require_once 'database_connection.php';
 
+$page = intval($_GET['page']);
+$count = intval($_GET['count']);
+$trimCount = ($count * $page) - $count;
+
 $query = sprintf("SELECT friends FROM users WHERE id = '%d'", 8);
 $result = $link->query($query);
 $row = $result->fetch_row();
 $array_id = explode(", ", $row[0]);
-$friends = [];
+$trim_array_id = [];
+$counter = 0;
 
 for ($i = 0; $i <= count($array_id) - 1; $i++) {
-  $query = sprintf("SELECT id, name, photo, status FROM users WHERE id = '%d'", $array_id[$i]);
+  if($i >= $trimCount) {
+    array_push($trim_array_id, $array_id[$i]);
+    $counter++;
+  }
+  if ($counter === $count) break;
+}
+
+$friends = [];
+
+for ($i = 0; $i <= count($trim_array_id) - 1; $i++) {
+  $query = sprintf("SELECT id, name, photo, status FROM users WHERE id = '%d'", $trim_array_id[$i]);
   $result = $link->query($query);
   $row = $result->fetch_assoc();
 
   array_push($friends, $row);
 }
 
-echo json_encode($friends);
+$data['totalCount'] = count($array_id) - 1;
+$data['friends'] = $friends;
+
+echo json_encode($data);
 
 ?>
